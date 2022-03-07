09d071d42740c0c51b9e0ca3391670910657d26d
elijahb22 <elibusick11@gmail.com>
2022-03-07 16:21:33 -0500

    created reply function and intergrated it to the front end

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addreply, removeReply } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaID').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId> 
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addreply).delete(removeComment);
 
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
